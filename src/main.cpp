ed2889744b798b2005709ffa088291727f938e13
David Gomes <david.gomes@tuta.io>
2024-11-11 19:27:26 -0300

    Feat: Update P and Q search function

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,34 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 #include <omp.h>
 
-bool is_prime(int n) {
-    if (n < 2) {
-        return false;
-    }
-
-    // using brute force to check if n is prime
-    for (int i = 2; i <= sqrt(n); i++) {
-        if (n % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
+/*
+  Dado um texto cifrado com o RSA e sabendo que p e q são números primos
+  menores que 1024 (10 bits), faça um programa que encontre a chave privada
+  d sendo que a chave pública {e , n} é conhecida.
+*/
 
 std::pair<int, int> factorize_n(int n) {
     int p = 0, q = 0;
-    // using brute force to find the factors of n
-    // based on the euclidean algorithm
-    for (int i = 2; i <= sqrt(n); i++) {
-        if (is_prime(i) && n % i == 0) {
-            int possible_q = n / i;
-            if (is_prime(possible_q)) {
-                p = i;
-                q = possible_q;
-                break;
-            }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            p = i;
+            q = n / p;
+            break;
         }
     }
+
+    if (!p || !q) {
+        std::cout << "Error: could not find p or q!\n";
+        exit(1);
+    }
+
     return std::make_pair(p, q);
 }
 
 int main() {
     int e, n;
-
 
     // reading the public key from the terminal
     std::cout << "Enter the public key (e, n): ";
@@ -46,11 +39,10 @@ int main() {
     std::cout << "p: " << p << std::endl;
     std::cout << "q: " << q << std::endl;
 
-    int phi_n = (p - 1) * (q - 1);
+    int phi_n = (p - 1) * (q - 1); // totient
 
     // finding the private key
     int d;
-
 
     return 0;
 }
